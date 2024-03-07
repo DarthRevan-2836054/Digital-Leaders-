50f56aa0e7c1ad6ebc169e5e6ef50d5e199c4851
DarthRevan-2836054 <160975541+DarthRevan-2836054@users.noreply.github.com>
2024-03-07 12:46:45 +0000

    Deleting the Script

--- Digital Leaders/script.js
+++ Digital Leaders/script.js
@@ -1,15 +1,1 @@
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    const square = entry.target.querySelector(".non-scroll");
 
-    if (entry.isIntersecting) {
-      square.classList.add('scrollImg');
-	  return; // if we added the class, exit the function
-    }
-
-    // We're not intersecting, so remove the class!
-    square.classList.remove('scrollImg');
-  });
-});
-
-observer.observe(document.querySelector(".IMAGE"));
